6d7c8cd43ef49f91a00ffd3e3a2a672ee83c0fd3
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-02-17 18:35:26 +0530

    a

--- main.tf
+++ main.tf
@@ -9,3 +9,39 @@ provider "google" {
   region = var.region
   zone   = var.zone
 }
+
+resource "google_compute_instance" "my-cicd-vm" {
+  name         = "cicd-vms"
+  machine_type = "e2-small"
+  project = "o-media-practice"
+  zone     = "us-central1-a"
+
+ 
+
+  tags = ["allow-firewall"]
+
+ 
+
+  
+
+ 
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+ 
+
+  network_interface {
+    network = "default"
+
+ 
+
+  }
+  allow_stopping_for_update = true
+}
