dbbf3b05af16b68b0ccd6bcaf453bb41acbaff1d
nehapatel345 <99860862+nehapatel345@users.noreply.github.com>
2023-02-28 17:41:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,3 +87,26 @@ resource "google_storage_bucket" "auto-1" {
     }
   }
 }
+resource "google_storage_bucket" "auto-1" {
+  name          = "auto-expiring-bucket123bbbcvbdfgq"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
